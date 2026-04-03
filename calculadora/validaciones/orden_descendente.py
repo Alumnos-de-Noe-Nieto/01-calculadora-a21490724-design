dd71904aa0c9aab0461123ba5e3e8b4521c1aecb
Felipe De Jesus Reyes Murillo <a21490724@itmexicali.edu.mx>
2026-04-03 00:18:45 -0700

    nievel 4 completado

--- calculadora/validaciones/orden_descendente.py
+++ calculadora/validaciones/orden_descendente.py
@@ -44,4 +44,25 @@ def validar_orden_descendente(cadena: str) -> bool:
         >>> validar_orden_descendente("VIV")
         False
     """
-    raise NotImplementedError()
+    valores = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sustracciones_validas = {'IV', 'IX', 'XL', 'XC', 'CD', 'CM'}
+
+    i = 0
+    ultimo_valor = float('inf')
+    while i < len(cadena):
+        par = cadena[i:i+2]
+        if par in sustracciones_validas:
+            if i > 0 and cadena[i-1] == cadena[i]:
+                return False
+            valor_par = valores[cadena[i+1]] - valores[cadena[i]]
+            if valor_par > ultimo_valor:
+                return False
+            ultimo_valor = valor_par
+            i += 2
+        else:
+            valor = valores[cadena[i]]
+            if valor > ultimo_valor:
+                return False
+            ultimo_valor = valor
+            i += 1
+    return True
